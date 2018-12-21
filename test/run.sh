c90c7cd4a604bdd9e4a8b1dd27a7712b57356037
Yibo Lin <yibolin@utexas.edu>
2018-12-21 17:53:12 -0600

    track placement for routing

--- test/run.sh
+++ test/run.sh
@@ -7,7 +7,7 @@
 
 if [ ! -n "$1" ]
 then
-  echo "Usage: `basename $0` circuit techfile spacing_rule width_area_rule enclosure_rule well_contact_GDSII"
+  echo "Usage: `basename $0` circuit techfile spacing_rule width_area_rule enclosure_rule well_contact_GDSII lef_file"
   return 
 fi  
 
@@ -18,6 +18,7 @@ TOP_SPACING_RULE_FILE=$3
 TOP_WIDTH_AREA_RULE_FILE=$4
 TOP_ENCLOSURE_RULE_FILE=$5
 TOP_WELL_CON_GDS_FILE=$6
+TOP_LEF_FILE=$7
 
 # get script directory instead of execution directory
 TOP_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -27,13 +28,13 @@ echo "CIRCUIT = ${TOP_CIRCUIT}"
 echo "CIRCUIT_NAME = ${TOP_CIRCUIT_NAME}"
 echo "TOP_SCRIPT_DIR = ${TOP_SCRIPT_DIR}"
 
-## run constraint generation 
-#echo "${TOP_CIRCUIT_NAME}: constraint generation"
-#source ${TOP_SCRIPT_DIR}/../constraint_generation/test/run.sh ${TOP_CIRCUIT}/${TOP_CIRCUIT_NAME}.sp
-#
-## run device generation 
-#echo "${TOP_CIRCUIT_NAME}: device generation"
-#source ${TOP_SCRIPT_DIR}/../placement/device_generation/test/run.sh ${TOP_CIRCUIT}
+# run constraint generation 
+echo "${TOP_CIRCUIT_NAME}: constraint generation"
+source ${TOP_SCRIPT_DIR}/../constraint_generation/test/run.sh ${TOP_RESULT_DIR}/${TOP_CIRCUIT}/${TOP_CIRCUIT_NAME}.sp
+
+# run device generation 
+echo "${TOP_CIRCUIT_NAME}: device generation"
+source ${TOP_SCRIPT_DIR}/../placement/device_generation/test/run.sh ${TOP_CIRCUIT}
 
 # run analog placement 
 echo "${TOP_CIRCUIT_NAME}: analog placement"
@@ -52,3 +53,15 @@ source ${TOP_SCRIPT_DIR}/../placement/well_generation/test/run.sh \
     ${TOP_ENCLOSURE_RULE_FILE} \
     ${TOP_WELL_CON_GDS_FILE} \
     ${TOP_RESULT_DIR}/${TOP_CIRCUIT}/result_legal_detail.txt
+
+# run analog routing 
+echo "${TOP_CIRCUIT_NAME}: analog routing"
+source ${TOP_SCRIPT_DIR}/../routing/test/run.sh \
+    ${TOP_RESULT_DIR}/${TOP_CIRCUIT_NAME}/${TOP_CIRCUIT_NAME}.wellgen.gds \
+    ${TOP_RESULT_DIR}/${TOP_CIRCUIT_NAME}/${TOP_CIRCUIT_NAME}.gr \
+    ${TOP_RESULT_DIR}/${TOP_CIRCUIT_NAME}/${TOP_CIRCUIT_NAME}.wcon \
+    ${TOP_RESULT_DIR}/${TOP_CIRCUIT_NAME}/${TOP_CIRCUIT_NAME}.sub \
+    ${TOP_RESULT_DIR}/${TOP_CIRCUIT_NAME}/${TOP_CIRCUIT_NAME}.iopin \
+    ${TOP_RESULT_DIR}/${TOP_CIRCUIT_NAME}/${TOP_CIRCUIT_NAME}.symnet \
+    ${TOP_LEF_FILE} \
+    ${TOP_TECHFILE}
