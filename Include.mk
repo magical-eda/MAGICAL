097dca2395ce99722685996ff055b544c3fab730
Keren Zhu <keren@cerc.utexas.edu>
2019-01-08 21:50:15 -0600

    Adds Include.mk

---
+++ Include.mk
@@ -0,0 +1,8 @@
+# ==========================================================================
+# general file for inclusion in sub-makefiles 
+# make sure required variables are already set before including this file 
+
+# define PREFIX for installation destination
+# default PREFIX is current folder 
+# it must be absolute path 
+PREFIX = $(realpath/install)
