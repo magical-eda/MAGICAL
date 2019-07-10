a59992e682ac148b879ebca0cbb1860f8072487a
Keren Zhu <keren@cerc.utexas.edu>
2019-07-10 16:48:07 -0500

    Writing files

--- flow/python/PnR.py
+++ flow/python/PnR.py
@@ -12,24 +12,115 @@ class PnR(object):
         self.dDB = magicalDB.designDB.db
         self.tDB = magicalDB.techDB
     
-    def implLayout(self, ckt_idx):
+    def implLayout(self, cktIdx, dirname):
         """
         @brief PnR a circuit in the designDB
         @param the index of subckt
         """
         return True
-    def placement(self):
+    def writeBlockFile(self, cktIdx, filename):
         """
-        @brief placement
+        @brief write .block file
         """
-        return True
-    def routing(self):
+        ckt = self.dDB.subCkt(cktIdx)
+        fout = open(filename, "w")
+        for nodeIdx in range(ckt.numNodes()):
+            node = ckt.node(nodeIdx)
+            subCkt = self.dDB.subCkt(node.graphIdx)
+            fout.write(node.name)
+            fout.write(" ")
+            fout.write(str(subckt.gdsData().bbox().xLen()))
+            fout.write(" ")
+            fout.write(str(subckt.gdsData().bbox().yLen()))
+            fout.write("\n")
+    def writeConnectionFile(self, cktIdx, filename):
         """
-        @brief routing
+        @brief write .connection file
         """
-        return True
-    def device_generation(self):
+        fout = open(filename, "w")
+        ckt = self.dDB.subCkt(cktIdx)
+        for netIdx in range(ckt.numNets()):
+            net = ckt.net(netIdx)
+            fout.write(net.name)
+            fout.write(" ")
+            for pinIdx in net.numPins():
+                pin = ckt.pin(pinIdx)
+                nodeIdx = pin.nodeIdx()
+                node = ckt.node(nodeIdx)
+                fout.write(node.name)
+                fout.write(" ")
+                fout.write(str(pin.intNetIdx))
+                fout.write(" ")
+            fout.write("\n")
+    def writeNetFile(self, cktIdx, filename):
         """
-        @brief device generation
+        @brief write .net file
         """
-        return True
+        fout = open(filename, "w")
+        ckt = self.dDB.subCkt(cktIdx)
+        for netIdx in range(ckt.numNets()):
+            net = ckt.net(netIdx)
+            fout.write(net.name)
+            fout.write(" ")
+            for pinIdx in net.numPins():
+                pin = ckt.pin(pinIdx)
+                nodeIdx = pin.nodeIdx()
+                node = ckt.node(nodeIdx)
+                fout.write(node.name)
+                fout.write(" 0 0 ")
+            fout.write("\n")
+    def writeOffsetFile(self, cktIdx, filename):
+        """
+        @brief write .offset file
+        """
+        fout = open(filename, "w")
+        ckt = self.dDB.subCkt(cktIdx)
+        for nodeIdx in range(ckt.numNodes()):
+            node = ckt.node(nodeIdx)
+            subCkt = self.dDB.subCkt(node.graphIdx)
+            fout.write(node.name)
+            fout.write(" ")
+            xLo = subCkt.gdsData().bbox().xLo
+            yLo = subCkt.gdsData().bbox().yLo
+            fout.write(str(float(xLo) / 1000))
+            fout.write(" ")
+            fout.write(str(float(yLo) / 1000))
+            fout.write("\n")
+    def writePinFile(self, cktIdx, filename):
+        """
+        @brief write .pin file
+        """
+        fout = open(filename, "w")
+        ckt = self.dDB.subCkt(cktIdx)
+        for nodeIdx in range(ckt.numNodes()):
+            node = ckt.node(nodeIdx)
+            subCkt = self.dDB.subCkt(node.graphIdx)
+            fout.write(node.name)
+            fout.write("\n")
+            offsetXLo = float(subCkt.gdsData().bbox().xLo) / 1000
+            offsetYLo = float(subCkt.gdsData().bbox().yLo) / 1000
+            for netIdx in range(subCkt.numNets()):
+                net = subCkt.net(netIdx)
+                shape = net.ioShape()
+                layer = net.ioLayer()
+                xLo = float(shape.xLo / 1000) 
+                yLo = float(shape.yLo / 1000) 
+                xHi = float(shape.xHi / 1000) 
+                yHi = float(shape.yHi / 1000) 
+                fout.write(str(netIdx))
+                fout.write("    1\n ")
+                fout.write("M")
+                fout.write(str(layer))
+                fout.write("    ((")
+                fout.write(str(xLo - offsetXLo))
+                fout.write(" ")
+                fout.write(str(yLo - offsetYLo))
+                fout.write(") (")
+                fout.write(str(xHi - offsetXLo))
+                fout.write(" ")
+                fout.write(str(yHi - offsetYLo))
+                fout.write("))\n")
+
+
+
+            
--- flow/python/Router.py
+++ flow/python/Router.py
@@ -5,9 +5,38 @@
 # @brief The class for interfacing routing
 #
 
+import MagicalDB
+import magicalFlow
+
 class Router(object):
-    def readBackDumbFile(self, filename):
+    def __init__(self, magicalDB):
+        self.mDB = magicalDB
+        self.tDB = magicalDB.techDB
+        self.dDB = magicalDB.designDB.db
+
+    def readBackDumbFile(self, filename, cktIdx):
         """
         @brief read back the for-integration dumb file 
         """
+        ckt = self.dDB.subCkt(cktIdx)
+        gdsData = ckt.gdsData()
+        with open(filename)  as fin:
+            lines = fin.readlines()
+            bboxLine = lines[0].split()
+            xLo = int(bboxLine[0])
+            yLo = int(bboxLine[1])
+            xHi = int(bboxLine[2])
+            yHi = int(bboxLine[3])
+            gdsData.setBBox(xLo, yLo, xHi, yHi)
+            for lineIdx in range(1, len(lines)):
+                line = lines[lineIdx].split()
+                netIdx = lineIdx - 1
+                net = ckt.net(netIdx)
+                layer = int(line[1])
+                xLo = int(line[2])
+                yLo = int(line[3])
+                xHi = int(line[4])
+                yHi = int(line[5])
+                net.setIoShape(xLo, yLo, xHi, yHi)
+                net.ioLayer = layer
 
