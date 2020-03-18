ef7ef5bcf7916ba2cd5bbead7f62baca4cffcd78
Keren Zhu <keren@cerc.utexas.edu>
2020-03-18 18:17:54 -0500

    add vddd

--- flow/python/Params.py
+++ flow/python/Params.py
@@ -21,7 +21,7 @@ class Params:
         self.simple_tech_file = "/home/local/eda09/keren/benchmarks/tech/techfile" # Input simple tech file
         self.techfile = "/home/local/eda09/keren/benchmarks/tech/techfile"
         self.lef = "/home/local/eda09/keren/benchmarks/tech/40vhv.lef"
-        self.vddNetNames = ["VDD", "vdd", "vdda"]
+        self.vddNetNames = ["VDD", "vdd", "vdda", "vddd"]
         self.vssNetNames = ["VSS", "GND", "vss", "gnd", "vssa"]
         self.resultDir = None
 
